5c3b0ba29956acbebc1a49b69b2dd837d2eb801d
Ryos, Suzuki <ryos@sinby.com>
2016-01-03 00:47:34 +0900

    add null cps-paser.

--- k-transfer/cps-parser.lisp
+++ k-transfer/cps-parser.lisp
@@ -7,6 +7,10 @@
             (:documentation "Make a new environment."))
 
 ;----------------------------------------------------------------
+(defmethod make-new-env ((parser cps-parser) env)
+  (cons `(()) env))
+
+;----------------------------------------------------------------
 (defun terminal-p (expr)
   (or 
     (eq :#t expr)
@@ -16,111 +20,125 @@
     (numberp expr)))
 
 ;----------------------------------------------------------------
-(defun make-prefix-func-name (prefix func-name)
-        (intern (format nil "~a-~a" prefix func-name)))
+(defmethod cps-primitive-p ((parser cps-parser) op)
+  (case op 
+    (:#t nil)
+    (:#f nil)
+
+    (:+  t)
+    (:-  t)
+    (:*  t)
+
+    (:>> t)
+    (:<< t)
+
+    (:<  t)
+    (:>  t)
+    (:>= t)
+    (:<= t)
+    (:=  t)
+    (:/=  t)
+
+    (:heap t)
+    (:record-set! t)
+    (:record-ref t)
+    (:stack t)
+    (:pop t)
+
+    (otherwise nil)))
+
+;----------------------------------------------------------------
+(defun compare-symbolp (sym)
+  (case sym
+    (:>  t)
+    (:<  t)
+    (:>= t)
+    (:<= t)
+    (:=  t)
+    (:/=  t)
+    (otherwise nil)))
 
 ;----------------------------------------------------------------
 (defmacro def-cps-func (func-name &rest func-define)
-  (let* ((make-env-func (make-prefix-func-name 'make-env func-name))
-         (make-expr-func (make-prefix-func-name 'make-expr func-name))
-         (args (car func-define))
-         (parser-pair (car args))
-         (parser (car parser-pair))
-         (expr (cadr args))
-         (env (caddr args))
+  (let* ((args (car func-define))
+           (parser-pair (car args))
+             (parser (car parser-pair))
+           (expr (cadr args))
+           (env (caddr args))
 
-         (func-body (cdr func-define))
-         (new-env (gensym))
-         (walk-result (gensym))
-         (new-expr (gensym)))
+         (func-body (cdr func-define)))
 
-    `(defmethod ,func-name ,args
-       (let* ((,new-env (,make-env-func ,parser ,expr ,env))
-              (,walk-result (let ((,env ,new-env)) ,@func-body))
-              (,new-expr (,make-expr-func ,parser ,expr ,env ,new-env ,walk-result)))
-         ,new-expr))))
-
-;----------------------------------------------------------------
-(defmacro def-env-func (func-name &rest func-define)
-  (let* ((make-env-func (make-prefix-func-name 'make-env func-name))
-        (args (car func-define))
-          (parser-pair (car args))
-            (parser (car parser-pair))
-          (expr (cadr args))
-          (env (caddr args))
-        (func-body (cdr func-define)))
-
-    `(defmethod ,make-env-func ,args ,@func-body)))
-
-;----------------------------------------------------------------
-(defmacro def-expr-func (func-name &rest func-define)
-  (let* ((make-expr-func (make-prefix-func-name 'make-expr func-name))
-        (args (car func-define))
-          (parser-pair (car args))
-            (parser (car parser-pair))
-          (expr (cadr args))
-          (env (caddr args))
-          (walk-result (cadddr args))
-        (func-body (cdr func-define)))
-
-    `(defmethod ,make-expr ,args ,@func-body)))
-
-;----------------------------------------------------------------
-(def-env-func cps-terminal ((parser cps-parser) expr env) env)
-(def-env-func cps-fixs ((parser cps-parser) expr env) env)
-(def-env-func cps-fixh ((parser cps-parser) expr env) env)
-(def-env-func cps-app ((parser cps-parser) expr env) env)
-(def-env-func cps-exit ((parser cps-parser) expr env) env)
-(def-env-func cps-primitive ((parser cps-parser) expr env) env)
+    `(defmethod ,func-name ,args ,@func-body)))
 
 ;----------------------------------------------------------------
 (def-cps-func cps-terminal ((parser cps-parser) expr env)
   expr)
 
 ;----------------------------------------------------------------
-(def-expr-func cps-terminal ((parser cps-parser) expr env walk-result) walk-result)
-
-;----------------------------------------------------------------
-
 (def-cps-func cps-bind ((parser cps-parser) expr env)
   (let ((func-name (car expr))
         (args (cadr expr))
         (next-cps (caddr expr)))
+    (let ((new-func-name (cps-terminal parser func-name env))
+          (new-args (mapcar #'(lambda (arg) (cps-terminal parser arg env)) args))
+          (new-next-cps (cps-parse parser next-cps env)))
 
-    (cps-parser parser next-cps bind env)
-    expr))
+      `(,func-name ,new-args ,new-next-cps))))
+
 ;----------------------------------------------------------------
-(def-cps-func cps-fixs ((parser cps-parser) expr env)
-  (let ((binds (cadr expr))
+(def-cps-func cps-fix ((parser cps-parser) expr env)
+  (let ((fix-op (car expr))
+        (binds (cadr expr))
         (next-cps (caddr expr)))
         
-    (cons (mapcar #'(lambda (bind) (cps-bind parser bind env)) binds)
-          (cps-parse parser next-cps env))))
+    (let ((new-binds (mapcar #'(lambda (bind) (cps-bind parser bind env)) binds))
+          (new-next-cps (cps-parse parser next-cps env)))
+
+      `(,fix-op ,new-binds ,new-next-cps))))
 ;----------------------------------------------------------------
 (def-cps-func cps-fixh ((parser cps-parser) expr env)
-  (let ((binds (cadr expr))
-        (next-cps (caddr expr)))
-        
-    (cons (mapcar #'(lambda (bind) (cps-bind parser bind env)) binds)
-          (cps-parse parser next-cps env))))
+  (cps-fix (parser expr env)))
+
+;----------------------------------------------------------------
+(def-cps-func cps-fixs ((parser cps-parser) expr env)
+  (cps-fix (parser expr env)))
+
 ;----------------------------------------------------------------
 (def-cps-func cps-app ((parser cps-parser) expr env)
-  (let ((op (cadr expr))
+  (let ((func-name (cadr expr))
         (args (caddr expr)))
         
-    (cons (mapcar #'(lambda (bind) (cps-parse parser op env)) binds)
-          (cps-parse parser args env))))
-(def-expr-func cps-fixs ((parser cps-parser) expr env walk-result) expr)
-(def-expr-func cps-fixh ((parser cps-parser) expr env walk-result) expr)
-(def-expr-func cps-app ((parser cps-parser) expr env walk-result) expr)
-(def-expr-func cps-exit ((parser cps-parser) expr env walk-result) expr)
-(def-expr-func cps-primitive ((parser cps-parser) expr env walk-result) expr)
+    (let ((new-func-name (cps-terminal parser func-name env))
+          (new-args (mapcar #'(lambda (arg) (cps-terminal parser arg env)) args)))
+      `(:APP ,new-func-name ,new-args))))
 ;----------------------------------------------------------------
+(def-cps-func cps-exit ((parser cps-parser) expr env)
+  (let ((arg0 (caadr expr)))
+    (let ((new-arg0 (cps-terminal parser expr env)))
+      `(:EXIT (,new-arg0) () ()))))
 
+;----------------------------------------------------------------
+(def-cps-func cps-primitive ((parser cps-parser) expr env)
+  (let ((op (car expr))
+        (args (cadr expr))
+        (result (caddr expr))
+        (next-cpss (cadddr expr)))
+
+    (let ((new-args (mapcar #'(lambda (arg) (cps-terminal parser arg env)) args))
+          (new-result (mapcar #'(lambda (r) (cps-terminal parser r env)) result))
+          (new-next-cpss (mapcar #'(lambda (cps) (cps-parse parser cps env)) next-cpss)))
+
+      `(,op ,new-args ,new-result ,new-next-cpss))))
+
+;----------------------------------------------------------------
+(defmethod cps-error-exit ((parser cps-parser) expr env)
+   (error 'parse-error :expr expr :env env))
+
+;----------------------------------------------------------------
 (defmethod cps-parse ((parser cps-parser) expr env)
 
   (if (terminal-p expr)
-    (cps-terminal parser expr context)
+    (cps-terminal parser expr env)
     (let ((op (car expr)))
       (case op
         (:fixs (cps-fixs parser expr env))
@@ -128,6 +146,8 @@
         (:app (cps-app parser expr env))
         (:neq (cps-neq parser expr env))
         (:exit (cps-exit parser expr env))
-        (otherwise (if (primitive? (parser op))
+        (otherwise (if (cps-primitive-p parser op)
                      (cps-primitive parser expr env)
-                     (cps-otherwise parser expr env)))))))
+                     (cps-error-exit parser expr env)))))))
+
+
