b3d82eb345f68d688b77fa0805d888af412bc70f
Ryos, Suzuki <ryos@sinby.com>
2016-01-04 00:45:05 +0900

    now debugging fixh.

--- k-transfer/closure-converter.lisp
+++ k-transfer/closure-converter.lisp
@@ -38,16 +38,6 @@
                      `(,:RECORD-REF (,closure-name ,no) (,var-name) ((,next-cps0))))))))
       (wrap-cps-with-record-ref0 free-vars 1 new-next-cps))))
 
-;----------------------------------------------------------------
-#|
-(defun wrap-cps-with-heap (heap-sym new-next-cps new-env)
-  (let ((free-vars (car new-env))
-                 (let* ((closure-name (car func-names0))
-                        (label0 `(:LABEL ,(make-new-func-name closure-name)))
-                        (stack-list (cons label0 
-                                          (copy-list free-vars))))
-           `(:HEAP ,free-vars (,heap-sym) (,closure-name) (,new-next-cps))))))
-|#
 
 ;----------------------------------------------------------------
 (def-cps-func cps-app ((parser closure-converter) expr env)
@@ -61,26 +51,36 @@
       `(:RECORD-REF (,closure-name 0) (,sym0) ((:APP ,sym0 (,closure-name ,@args)))))))
 
 ;----------------------------------------------------------------
+#|
 (def-cps-func cps-bind ((parser closure-converter) expr env)
-  (let ((func-name (car expr))
+  (let ((closure-name (car expr))
         (args (cadr expr))
         (next-cps (caddr expr)))
 
-    (let ((new-func-name (make-new-func-name func-name))
+    (print `(func-name-args ,args))
+
+    (let ((func-name (make-new-func-name closure-name))
+          (new-args (cons closure-name args))
           (new-next-cps (cps-parse parser next-cps env)))
 
-      `(,new-func-name ,args ,(wrap-cps-with-record-ref func-name new-next-cps env)))))
+      `(,func-name ,new-args ,(wrap-cps-with-record-ref closure-name new-next-cps env)))))
+|#
 
 ;----------------------------------------------------------------
 (def-cps-func cps-bind-fixs ((parser closure-converter) expr env)
-  (let ((func-name (car expr))
+  (let ((closure-name (car expr))
         (args (cadr expr))
         (next-cps (caddr expr))
 
         (free-vars (env-to-free-variables env)))
 
-    (let ((wrapped-cps `(,func-name ,args (:POP (,(+ (length free-vars) 1)) () (,next-cps)))))
-      (cps-bind parser wrapped-cps env))))
+    (let* ((func-name (make-new-func-name closure-name))
+           (new-args (cons closure-name args))
+           (new-next-cps (cps-parse parser next-cps env))
+           (pop-cps `(:POP (,(+ (length free-vars) 1)) () (,new-next-cps)))
+           (wrapped-cps (wrap-cps-with-record-ref closure-name pop-cps env)))
+
+      `(,func-name ,new-args ,wrapped-cps))))
 
 ;----------------------------------------------------------------
 (def-cps-func cps-fixs ((parser closure-converter) expr env)
@@ -90,10 +90,12 @@
         (finder (make-instance 'free-variable-finder)))
 
     (let ((finder-env (make-new-env finder '())))
-      (cps-parse finder expr finder-env)
+      (dolist (bind binds)
+        (cps-bind finder bind finder-env))
+
       (let* ((free-variables
               (remove-if #'null (mapcar #'(lambda (x) (if (null (cdr x)) (car x))) (car finder-env))))
-            (new-env (make-new-env parser env (cons :fixs free-variables))))
+            (new-env (make-new-env parser env (cons `(:fixs) free-variables))))
 
         (let ((new-binds (mapcar #'(lambda (bind) (cps-bind-fixs parser bind new-env)) binds))
               (new-next-cps (cps-parse parser next-cps new-env))
@@ -103,26 +105,50 @@
             `(,fix-op ,new-binds ,wrapped-cps)))))))
 
 ;----------------------------------------------------------------
-#|
+(def-cps-func cps-bind-fixh ((parser closure-converter) expr env)
+  (let ((closure-name (car expr))
+        (args (cadr expr))
+        (next-cps (caddr expr))
+
+        (free-vars (env-to-free-variables env)))
+
+    (let* ((func-name (make-new-func-name closure-name))
+           (new-args (cons closure-name args))
+           (new-next-cps (cps-parse parser next-cps env)))
+
+      `(,func-name ,new-args ,new-next-cps))))
+
+;----------------------------------------------------------------
+(defun wrap-cps-with-heap (func-names heap-closure-sym expr)
+  (labels ((wrap-cps-with-heap0 (func-names0 expr0)
+             (if (null func-names0) expr0
+               (let* ((closure-name (car func-names0))
+                      (label0 `(:LABEL ,(make-new-func-name closure-name)))
+                      (heap-list `(,label0 ,heap-closure-sym)))
+                 (wrap-cps-with-heap0 (cdr func-names0) `(:HEAP ,heap-list (,closure-name) (,expr0)))))))
+  (wrap-cps-with-heap0 (reverse func-names) expr)))
+
+;----------------------------------------------------------------
 (def-cps-func cps-fixh ((parser closure-converter) expr env)
   (let ((fix-op (car expr))
         (binds (cadr expr))
         (next-cps (caddr expr))
-        (new-env (make-new-env parser env))
         (finder (make-instance 'free-variable-finder)))
 
     (let ((finder-env (make-new-env finder '()))
-          (heap-closure-sym (cps-gensym)))
+          (heap-closure-sym (cps-gensym parser))
+          (func-names (mapcar #'(lambda (x) (car x)) binds)))
 
       (cps-parse finder expr finder-env)
-      (let ((free-variables
-              (remove-if #'null (mapcar #'(lambda (x) (if (null (cdr x)) (car x))) (car finder-env)))))
-        (setf (car new-env) (cons `(:fixh ,heap-closure-sym) free-variables))
+      (let* ((free-variables
+               (remove-if #'null (mapcar #'(lambda (x) (if (null (cdr x)) (car x))) (car finder-env))))
+             (fixh-free-vars (cons `(:fixh ,func-names) free-variables))
+             (new-env (make-new-env parser env free-variables))
+             (new-binds (mapcar #'(lambda (bind) (cps-bind-fixh parser bind new-env)) binds))
 
-        (let ((new-binds (mapcar #'(lambda (bind) (cps-bind-fixs parser bind new-env)) binds))
-              (new-next-cps (cps-parse parser next-cps new-env))
-              (func-names (mapcar #'(lambda (x) (car x)) binds)))
+             (new-next-cps (cps-parse parser next-cps new-env))
+             (new-expr `(:FIXH ,new-binds ,new-next-cps))
+             (wrapped-cps (wrap-cps-with-heap func-names heap-closure-sym new-expr)))
 
-          (let ((wrapped-cps (wrap-cps-with-heap func-names new-next-cps new-env)))
-            `(,fix-op ,new-binds ,wrapped-cps))))))
-  |#
+            `(:HEAP ,free-variables (,heap-closure-sym) (,wrapped-cps))))))
+
--- k-transfer/test.lisp
+++ k-transfer/test.lisp
@@ -1,5 +1,10 @@
 (load "closure-converter.lisp")
 (setf conv (make-instance 'closure-converter))
 (setf env (make-new-env conv '()))
+#|
 (print `(:cps-fixs
-  ,(cps-fixs conv '(:fixs ((c (r) (:+ (x r) (t) ((:app k (t)))))) (:app g (c x))) env)))
+  (cps-fixs conv '(:fixs ((c (r) (:+ (x r) (t) ((:app k (t)))))) (:app g (c x))) env)))
+|#
+
+(print `(:cps-fixh
+  ,(cps-fixh conv '(:fixh ((c (r) (:+ (x r) (t) ((:app k (t)))))) (:app g (c x))) env)))
