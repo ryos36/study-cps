f066b86411e66eee4a2f4b33b1bc07de00df20e5
Ryos, Suzuki <ryos@sinby.com>
2016-01-18 11:50:42 +0900

    test-050.scm test-042.cps are passed.

--- scm-script/test-049.scm
+++ scm-script/test-049.scm
@@ -12,5 +12,15 @@
          (fact (k n)
            (fact0 k n 1)))
 
+        (fact 10))
+
+ (:fix ((fact (k n)
+          (:fix ((fact0 (k n rv)
+                    (:if (:= n 0)
+                         rv
+                         (fact0 k (:- n 1) (:* n rv)))))
+                 (fact0 k n 1))))
+
         (fact 10)))
+
 
