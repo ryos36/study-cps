13ab3ea215048a26bba407e0866e935fd6d07f13
Ryos, Suzuki <ryos@sinby.com>
2016-01-18 13:18:22 +0900

    results are updated.

--- k-transfer/test.lisp
+++ k-transfer/test.lisp
@@ -27,6 +27,6 @@
 
 ;(cps-parse-one '(:fixs ((c (r) (:+ (x r) (t) ((:app k (t)))))) (:app g (c x))) *env*)
 
-(set-test-files '("42" "30" (14 . 29) (31 . 39)))
+(set-test-files '((14 . 29) (31 . 39) (40 . 42)))
 (do-test)
 
